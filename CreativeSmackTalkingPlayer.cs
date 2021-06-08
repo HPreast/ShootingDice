b45b595489b6a9f48425a236d45045ec97ffb676
HPreast <preasths1@gmail.com>
2021-06-08 13:57:30 -0500

    CreativeSmackTalkPlayer chooses a random taunt out of a list during each roll

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,19 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        public List<string> taunts = new List<string>(4) { "The keys you're looking for are Alt + F4.", "A moment of silence for your E-Sports career.", "I can't hear you from the bottom of the scoreboard.", "With all these flavors why do you choose salt?" };
 
+        public override int Roll()
+        {
+            int randomTaunt = new Random().Next(0, 3);
+            string smackTalk = taunts[randomTaunt];
+            Console.WriteLine(smackTalk);
+            return base.Roll();
+        }
     }
 }
